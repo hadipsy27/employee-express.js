d5e3d9be22b5f8932523fe95b5e655d5647f8031
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-06-04 10:03:42 +0700

    refactor auto and remove table to false and Remove unused library cors

--- Employee-backend/app.js
+++ Employee-backend/app.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 
 const app = express();
-
-var corsOptions = {
-    origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
 const db = require("./models");
-db.sequelize.sync({ force: true }) // force: true -> drop existing tables and re-sync database
+db.sequelize.sync({ force: false }) // if = force: true -> drop existing tables and re-sync database
     .then(() => {
         console.log("Synced db.");
     })
