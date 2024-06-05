fdb8205ecaef2531f631f8658099856c891bf0f0
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-06-05 15:13:51 +0700

    update database configuration with environment

--- Employee-backend/config/db.config.js
+++ Employee-backend/config/db.config.js
@@ -1,13 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "123",
-    DB: "data_kepegawaian",
-    dialect: "postgres",
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+        max: process.env.DB_MAX,
+        min: process.env.DB_MIN,
+        acquire: process.env.DB_ACQUIRE,
+        idle: process.env.DB_IDLE
     }
 };
